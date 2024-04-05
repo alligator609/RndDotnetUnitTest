26eb3e1043278b1cc3d4a8d1444d788ebc89d4e4
Zia Uddin Ahmed <zia.ete10@gmail.com>
2024-04-05 11:30:25 +0600

    UPDATE: Added Network Service, Adds Flent Assertion, Adds Xunit project, Test string and int

---
+++ NetworkUtility.Tests/PingTests/NetworkServiceTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NetworkUtility.Ping;
+
+namespace NetworkUtility.Tests.PingTests
+{
+    public class NetworkServiceTests
+    {
+        [Fact]
+        public void NetworkService_SendPing_ReturnString()
+        {
+            // Arrange
+            var networkService = new NetworkService();
+            var host = "localhost";
+            // Act
+            var result = networkService.SendPing(host);
+
+            // Assert
+            result.Should().NotBeNullOrEmpty();
+            result.Should().Be("Pinging " + host);
+            result.Should().Contain(host, Exactly.Once());
+        }
+        [Theory]
+        [InlineData(1, 2, 3)]
+        [InlineData(2, 3, 5)]
+        public void NetworkService_PingTime_ReturnInt(int a, int b, int expected)
+        {
+            // Arrange
+            var networkService = new NetworkService();
+            // Act
+            var result = networkService.PingTime(a, b);
+
+            // Assert
+            result.Should().Be(expected);
+            result.Should().BePositive();
+            result.Should().BeGreaterOrEqualTo(3);
+        }
+    } 
+}
---
+++ NetworkUtility/Ping/NetworkService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkUtility.Ping
+{
+    public class NetworkService
+    {
+        public string SendPing(string host)
+        {
+            return "Pinging " + host;
+        }
+        public int PingTime(int a, int b )
+        {
+            return a + b;
+        }
+    }
+}
