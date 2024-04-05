70d3f9e1def37731db8e40b00385ee870dd965be
Zia Uddin Ahmed <zia.ete10@gmail.com>
2024-04-05 23:05:49 +0600

    UPDATE: Adds date time, object, IEmuarble

--- NetworkUtility.Tests/PingTests/NetworkServiceTests.cs
+++ NetworkUtility.Tests/PingTests/NetworkServiceTests.cs
@@ -1,23 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Net.NetworkInformation;
 using FluentAssertions;
+using FluentAssertions.Extensions;
 using NetworkUtility.Ping;
 
 namespace NetworkUtility.Tests.PingTests
 {
     public class NetworkServiceTests
     {
+        private readonly NetworkService _networkService;
+        public NetworkServiceTests()
+        {
+            //SUT = System Under Test
+            _networkService = new NetworkService();
+        }
+
         [Fact]
         public void NetworkService_SendPing_ReturnString()
         {
-            // Arrange
-            var networkService = new NetworkService();
+            // Arrange          
             var host = "localhost";
             // Act
-            var result = networkService.SendPing(host);
+            var result = _networkService.SendPing(host);
 
             // Assert
             result.Should().NotBeNullOrEmpty();
@@ -30,14 +33,69 @@ namespace NetworkUtility.Tests.PingTests
         public void NetworkService_PingTime_ReturnInt(int a, int b, int expected)
         {
             // Arrange
-            var networkService = new NetworkService();
+
             // Act
-            var result = networkService.PingTime(a, b);
+            var result = _networkService.PingTime(a, b);
 
             // Assert
             result.Should().Be(expected);
             result.Should().BePositive();
             result.Should().BeGreaterOrEqualTo(3);
         }
-    } 
+
+
+        [Fact]
+        public void NetworkService_LastPingDate_ReturnDateTime()
+        {
+            // Arrange
+
+            // Act
+            var result = _networkService.LastPingDate();
+
+            // Assert
+            result.Should().BeAfter(1.January(2020));
+            result.Should().BeBefore(1.January(2026));
+        }
+
+        [Fact]
+        public void NetworkService_LastPingDate_ReturnObject()
+        {
+            // Arrange
+            var expected = new PingOptions
+            {
+                DontFragment = true,
+                Ttl = 1
+            };
+
+            // Act
+            var result = _networkService.GetPingOptions();
+
+            // Assert
+            result.Should().BeOfType<PingOptions>();
+            result.Should().BeEquivalentTo(expected);
+            result.Ttl.Should().Be(1);
+
+        }
+
+        [Fact]
+        public void NetworkService_MostRecentPings_ReturnIEnumerable()
+        {
+            // Arrange
+               var expected = new PingOptions
+                {
+                    DontFragment = true,
+                    Ttl = 1
+                };
+            // Act
+            var result = _networkService.MostRecentPings();
+
+            // Assert
+            result.Should().NotBeNull(); // Ensure result is not null
+            result.Should().BeAssignableTo<IEnumerable<PingOptions>>(); // Check if result is of type IEnumerable<PingOptions>
+            result.Should().ContainEquivalentOf(expected); // Check if result contains an item equivalent to 'expected'
+            result.Should().OnlyContain(x => x.DontFragment == true && x.Ttl == 1); // Ensure all items have the expected properties
+            result.Should().HaveCount(3); // Ensure there are exactly 3 items in the result
+
+        }
+    }
 }
