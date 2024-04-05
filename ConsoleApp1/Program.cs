a8f966f03cc84ce7cdff12bc253426154dfcf67e
Zia Uddin Ahmed <zia.ete10@gmail.com>
2024-04-05 10:38:32 +0600

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1.Tests;
+
+Console.WriteLine("Hello, World!");
+
+DumbFunctionsTests.ReturnsPikachu_WhenGiven1_ReturnsPikachu();
---
+++ ConsoleApp1/Tests/DumbFunctionsTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Tests
+{
+    public class DumbFunctionsTests
+    {
+        public static void ReturnsPikachu_WhenGiven1_ReturnsPikachu()
+        {
+            // Arrange => go get your things, class variables ,  
+            int num = 1;
+
+            // Act  => Call the function you wanna test
+            string result = DumbFunctions.ReturnsPikachu(num);
+
+            // Assert => Check if this is the actual result you want 
+            if (result == "Pikachu!")
+            {
+                Console.WriteLine("Test Passed");
+            }
+            else
+            {
+                Console.WriteLine("Test Failed");
+            }
+        }
+    }
+}
